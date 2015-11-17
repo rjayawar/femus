5321dba08e1717697dce48e6dc063af387d6d75e
Student <rangana.jayawardhana@ttu.edu>
2015-11-17 11:39:58 -0600

    Mesh generation from function instead of input file

--- applications/app_rangana/app_rangana.cpp
+++ applications/app_rangana/app_rangana.cpp
@@ -24,8 +24,8 @@ bool SetBoundaryCondition(const std::vector < double >& x, const char solName[],
   bool dirichlet = true; //dirichlet
   value = 0;
 
-  if (faceName == 2)
-    dirichlet = false;
+//   if (faceName == 2)
+//     dirichlet = false;
 
   return dirichlet;
 }
@@ -50,10 +50,11 @@ int main(int argc, char** args) {
   MultiLevelMesh mlMsh;
   double scalingFactor = 1.;
   // read coarse level mesh and generate finers level meshes
-  mlMsh.ReadCoarseMesh("./input/square.neu", "seventh", scalingFactor);
+  //mlMsh.ReadCoarseMesh("./input/square.neu", "seventh", scalingFactor);
+  mlMsh.GenerateCoarseBoxMesh( 8,8,0,-0.5,0.5,-0.5,0.5,0.,0.,QUAD9,"seventh");
   /* "seventh" is the order of accuracy that is used in the gauss integration scheme
       probably in the furure it is not going to be an argument of this function   */
-  unsigned numberOfUniformLevels = 3;
+  unsigned numberOfUniformLevels = 1;
   unsigned numberOfSelectiveLevels = 0;
   mlMsh.RefineMesh(numberOfUniformLevels , numberOfUniformLevels + numberOfSelectiveLevels, NULL);
   mlMsh.PrintInfo();
